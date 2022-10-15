098678f05d2590d9f1a3865739436028fd742f63
a-fly-fly-bird <lucas.y.tan.life@outlook.com>
2022-10-15 15:59:40 +0800

    cmake file

---
+++ 课后实践2/build/CMakeFiles/homework_1.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/tanyong/Documents/School/大四上/A02-车载软件开发基础/code/课后实践2/main.cpp" "CMakeFiles/homework_1.dir/main.cpp.o" "gcc" "CMakeFiles/homework_1.dir/main.cpp.o.d"
+  "/Users/tanyong/Documents/School/大四上/A02-车载软件开发基础/code/课后实践2/smartCar.cpp" "CMakeFiles/homework_1.dir/smartCar.cpp.o" "gcc" "CMakeFiles/homework_1.dir/smartCar.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
